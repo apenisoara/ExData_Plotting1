5fc90a2eb810db6a22a03d23de2c6914d5770982
Adrian Penisoara <apenisoara@gmail.com>
2014-05-12 01:37:05 +0200

    First attempt at 3rd plot

---
+++ plot3.R
@@ -0,0 +1,14 @@
+data=read.csv("dataset.txt",sep=";",dec=".",
+              colClasses=c(rep("character",2),rep("numeric",7)),
+              na.strings="?")
+datetime<-strptime(paste(data$Date,data$Time),format="%d/%m/%Y %H:%M:%S")
+
+png(file="plot3.png", width=480, height=480)
+plot(datetime,data$Sub_metering_1,type="l",
+     xlab="",
+     ylab="Energy sub metering",
+     main="")
+lines(datetime,data$Sub_metering_2,col="red")
+lines(datetime,data$Sub_metering_3,col="blue")
+legend("topright",pch="-",col=c("black","red","blue"), legend=names(data)[7:9])
+dev.off()
