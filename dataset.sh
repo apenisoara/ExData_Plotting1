cdf683d36a3e67f2bc146b63f3d701f61bf26338
Adrian Penisoara <apenisoara@gmail.com>
2014-05-12 17:30:58 +0200

    Make dataset.sh file executable, comment it.

--- dataset.sh
+++ dataset.sh
@@ -1,9 +1,14 @@
-# How the dataset file was generated
+#!/bin/sh
+
+# Input data setup
 INFILE=household_power_consumption.txt
 INFILE_ZIP=${INFILE%.txt}.zip
 OUTFILE=dataset.txt
 
+# Download the file and uncompress the target
 curl -o $INFILE_ZIP --url "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
 unzip $INFILE_ZIP $INFILE
+
+# How the dataset file was extracted
 head -1 $INFILE > $OUTFILE
 grep "^[12]/2/2007" $INFILE >> $OUTFILE
