0d5942f496ad5b2b8d79651224b77b8069b5abd1
apenisoara <apenisoara@users.noreply.github.com>
2014-05-12 10:46:05 +0200

    Complete with missing steps
    * add unzip and heading extraction steps
    * cosmetic rework / parametrization

--- dataset.sh
+++ dataset.sh
@@ -1,3 +1,9 @@
 # How the dataset file was generated
-curl -o household_power_consumption.txt --url "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
-grep "^[12]/2/2007" household* >>dataset.txt
+INFILE=household_power_consumption.txt
+INFILE_ZIP=${INFILE%.txt}.zip
+OUTFILE=dataset.txt
+
+curl -o $INFILE_ZIP --url "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+unzip $INFILE_ZIP $INFILE
+head -1 $INFILE > $OUTFILE
+grep "^[12]/2/2007" $INFILE >> $OUTFILE
